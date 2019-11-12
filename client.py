874c6c74733d520abc02592c0576dcb3eec32762
ChandraKiranSaladi <kiranck18@gmail.com>
2019-11-12 12:52:55 -0600

    Working code until Step2 of the protocol.

--- client.py
+++ client.py
@@ -1,62 +1,117 @@
 import socket
 import sys
-import socket
-import sys
+import pandas
 from Crypto.PublicKey import RSA
-import zlib
-import base64
 from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Signature import pss
+from Crypto.Util.strxor import strxor
+from Crypto.Util.Padding import pad,unpad
+from Crypto.Hash import SHA256
+import zlib
+import uuid
 
-def encrypt_file(public_key, file_data):
-    rsa_key = RSA.importKey(public_key)
-    rsa_key = PKCS1_OAEP.new(rsa_key)
+class Client:
 
-    #compress the data first
-    file_data = zlib.compress(file_data)
-    #will encrypt and decrypt chunks at a time
-    chunk_size = 470
-    offset = 0
-    end_loop = False
-    encrypted = ""
-    while not end_loop:
-        #The chunk
-        chunk = file_data[offset:offset + chunk_size]
+    def server_authentication(self,public_key,conn):
+        """
+        Authenticates server to the client
+        --- 
+        * public_key: RSA Public key
+        * conn: socket connection
 
-        #If the data chunk is less then the chunk size, then we need to add
-        #padding with " ". This indicates the we reached the end of the file
-        #so we end loop here
-        if len(chunk) % chunk_size != 0:
-            end_loop = True
-            chunk += " " * (chunk_size - len(chunk))
+        Returns:
 
-        #Append the encrypted chunk to the overall encrypted file
-        encrypted += rsa_key.encrypt(chunk)
+        * session Key -> bytes
 
-        #Increase the offset by chunk size
-        offset += chunk_size
+        """
+        rsa_public_key = RSA.importKey(public_key)
+        rsa_key = PKCS1_OAEP.new(rsa_public_key)
+        Na = uuid.uuid4()
+        print("Na ",Na)
+        initial_conn_string = rsa_key.encrypt(Na.bytes+"Alice".encode())
+        print(initial_conn_string)
 
-    #Base 64 encode the encrypted file
-    return base64.b64encode(encrypted)
+        # return initial_conn_string
+        # conn.send(initial_conn_string)
+        msg,integrity = conn.recv()
 
-s = socket.socket()
-s.connect(("localhost",5543))
-print("Connection established")
-print('Encrypting file...')
-f_pubk = open('public_key.pem', 'rb')
-public_key = f_pubk.read()
-f_pubk.close()
-f_file = open('input_directory/izuku.jpg', 'rb')
-data= f_file.read()
-f_file.close()
-encrypted_data = encrypt_file(public_key, data)
-fd = open("encrypted_img.jpg", "wb")
-fd.write(encrypted_data)
-fd.close()
+        #signature verification, if wrong throws a value error
+        # TODO: catch for Value Errors
+        pss.new(rsa_public_key).verify(SHA256.new(msg), integrity)
 
-f = open ("/home/rik/netsec/Secure-File-Transfer-Application/input_directory/izuku.jpg", "rb")
-s.send('encrypted_img.jpg'.encode('utf-8'))
-l = f.read(1024)
-while (l):
-    s.send(l)
-    l = f.read(1024)
-s.close()
+        sha = SHA256.new("Alice".encode() + Na.bytes).digest()
+        Nb =  unpad(strxor(sha,msg),32)
+        session_key = strxor(Na,Nb)
+
+        return session_key
+
+        
+    def encrypt_file(self,public_key, file_data):
+
+        #compress the data first
+        file_data = zlib.compress(file_data)
+        #will encrypt and decrypt chunks at a time
+        chunk_size = 470
+        offset = 0
+        end_loop = False
+        encrypted = ""
+        while not end_loop:
+            #The chunk
+            chunk = file_data[offset:offset + chunk_size]
+
+            #If the data chunk is less then the chunk size, then we need to add
+            #padding with " ". This indicates the we reached the end of the file
+            #so we end loop here
+            if len(chunk) % chunk_size != 0:
+                end_loop = True
+                chunk += " " * (chunk_size - len(chunk))
+
+            #Append the encrypted chunk to the overall encrypted file
+            encrypted += rsa_key.encrypt(chunk)
+
+            #Increase the offset by chunk size
+            offset += chunk_size
+
+        #Base 64 encode the encrypted file
+        # return base64.b64encode(encrypted)
+
+    def initiate_connection(self):
+        """
+        Initiates socket connection
+
+        Returns:
+
+         - s: socket
+        """
+        s = socket.socket()
+        s.connect(("localhost",5543))
+        print("Connection established")
+        return s
+
+def __init__():
+    client = Client()
+    s = client.initiate_connection()
+    f_pubk = open('public_key.pem', 'rb')
+    public_key = f_pubk.read()
+    f_pubk.close()
+    session_key = client.server_authentication(public_key,s)
+    
+
+# print('Encrypting file...')
+# rsa_key = RSA.importKey(public_key)
+# initial_string = rsa_key.encrypt(Na+"Alice")
+# f_file = open('input_directory/izuku.jpg', 'rb')
+# data= f_file.read()
+# f_file.close()
+# encrypted_data = encrypt_file(public_key, data)
+# fd = open("encrypted_img.jpg", "wb")
+# fd.write(encrypted_data)
+# fd.close()
+
+# f = open ("/home/rik/netsec/Secure-File-Transfer-Application/input_directory/izuku.jpg", "rb")
+# s.send('encrypted_img.jpg'.encode('utf-8'))
+# l = f.read(1024)
+# while (l):
+#     s.send(l)
+#     l = f.read(1024)
+# s.close()
--- server.py
+++ server.py
@@ -1,13 +1,24 @@
 import socket
 import sys
 from Crypto.PublicKey import RSA
-import zlib
-import base64
 from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Util.strxor import strxor
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad,unpad
+from Crypto.Signature import pss
+from Crypto.Hash import SHA256
+import zlib
+import uuid
+
 '''
 Generate RSA public/private key of server
 '''
 def generate_key():
+    """
+        Generates RSA Public Private key pair and stores it in public_key.pem \n
+        and private_key.pem files in the current directory
+
+    """
     print('inside generate key')
     new_key = RSA.generate(4096, e=65537)
     private_key = new_key.exportKey("PEM")
@@ -25,63 +36,97 @@ def generate_key():
     fd.write(public_key)
     fd.close()
 
-def encrypt_file(public_key, file_data):
-    rsa_key = RSA.importKey(public_key)
-    rsa_key = PKCS1_OAEP.new(rsa_key)
+class Server(socket):
+    
+    def send_ack_initial_connection(self,encrypted_msg,private_key,conn):
+        """
+        * Responds to the client connection
 
-    #compress the data first
-    file_data = zlib.compress(file_data)
-    #will encrypt and decrypt chunks at a time
-    chunk_size = 470 
-    offset = 0 
-    end_loop = False
-    encrypted = ""
-    while not end_loop:
-        #The chunk
-        chunk = file_data[offset:offset + chunk_size]
+        """
+        rsa_private_key = RSA.importKey(private_key)
+        rsa_key = PKCS1_OAEP.new(rsa_private_key)
+        Nb = uuid.uuid4()
+        # msg = conn.receive(512)
+        msg = rsa_key.decrypt(encrypted_msg)
+        name = msg[-5:]
+        print(name)
+        # if msg[-5:] != "Alice":
+        #     conn.close()
+        Na = msg[:-5]
+        print("Na recv", Na)
+        msg = name + Na
+        sha = SHA256.new(msg)
+        print("len sha ",len(sha.digest()) )
+        # print("len Nb ",len(Nb.encode()))
+        print("pad length ",len(pad(Nb.bytes,32)))
+        msg = strxor(sha.digest(),pad(Nb.bytes,32))
+        integrity = pss.new(rsa_private_key).sign(SHA256.new(msg))
+        length = len(msg) + len(integrity)
+        conn.send(length)
 
-        #If the data chunk is less then the chunk size, then we need to add
-        #padding with " ". This indicates the we reached the end of the file
-        #so we end loop here
-        if len(chunk) % chunk_size != 0:
-            end_loop = True
-            chunk += " " * (chunk_size - len(chunk))
+    # return msg
+    # conn.send(rsa_key.encrypt(msg))
 
-        #Append the encrypted chunk to the overall encrypted file
-        encrypted += rsa_key.encrypt(chunk)
+    def encrypt_file(self,public_key, file_data):
+        rsa_key = RSA.importKey(public_key)
+        rsa_key = PKCS1_OAEP.new(rsa_key)
 
-        #Increase the offset by chunk size
-        offset += chunk_size
+        #compress the data first
+        file_data = zlib.compress(file_data)
+        #will encrypt and decrypt chunks at a time
+        chunk_size = 470 
+        offset = 0 
+        end_loop = False
+        encrypted = ""
+        while not end_loop:
+            #The chunk
+            chunk = file_data[offset:offset + chunk_size]
 
-    #Base 64 encode the encrypted file
-    return base64.b64encode(encrypted)
+            #If the data chunk is less then the chunk size, then we need to add
+            #padding with " ". This indicates the we reached the end of the file
+            #so we end loop here
+            if len(chunk) % chunk_size != 0:
+                end_loop = True
+                chunk += " " * (chunk_size - len(chunk))
 
-s = socket.socket()
-s.bind(("localhost",5543))
-s.listen(10)
-generate_key()
-while True:
-    sock, address = s.accept()
+            #Append the encrypted chunk to the overall encrypted file
+            encrypted += rsa_key.encrypt(chunk)
 
-    print("Connection accepted from ",address)
+            #Increase the offset by chunk size
+            offset += chunk_size
 
-    #File Name
-    file_name = sock.recv(1024).decode('utf-8')
-    print('file received with file_name')
-    print(file_name)
+        #Base 64 encode the encrypted file
+        # return base64.b64encode(encrypted)
 
-    f= open("/home/rik/netsec/Secure-File-Transfer-Application/output_directory/"+file_name,'wb')
-    l = 1
-    while (l):       
-        # receive data and write it to file
-        l =  sock.recv(1024)
-        while (l):
-                f.write(l)
-                l =  sock.recv(1024)
-        print("I'm Done Here ")
+def __init__():
+    s = socket.socket()
+    s.bind(("localhost",5543))
+    s.listen(10)
+    while True:
+        sock, address = s.accept()
+        print("Connection accepted from ",address)
+        server = Server(sock)
+        server.send_ack_initial_connection
 
-        
-                
-        
 
-s.close()
+# generate_key()
+# while True:
+#     sock, address = s.accept()
+
+#     print("Connection accepted from ",address)
+
+#     #File Name
+#     file_name = sock.recv(1024).decode('utf-8')
+#     print('file received with file_name')
+#     print(file_name)
+
+#     f= open("/home/rik/netsec/Secure-File-Transfer-Application/output_directory/"+file_name,'wb')
+#     l = 1
+#     while (l):       
+#         # receive data and write it to file
+#         l =  sock.recv(1024)
+#         while (l):
+#                 f.write(l)
+#                 l =  sock.recv(1024)
+#         print("I'm Done Here ")
+# s.close()
