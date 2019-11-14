ae50de6649e0d41f4f08f4650dcc3e93d1164be8
ChandraKiranSaladi <kiranck18@gmail.com>
2019-11-14 14:05:14 -0600

    Updated protocol and need to add receive file

--- client.py
+++ client.py
@@ -12,7 +12,16 @@ import random
 
 class Client:
 
-    def server_authentication(self,public_key,conn):
+    def __init__(self):
+        self.socket = ''
+
+    def set_keys(self,k1,k2,k3,k4):
+        self.k1 = k1
+        self.k2 = k2
+        self.k3 = k3
+        self.k4 = k4
+
+    def server_authentication(self,public_key):
         """
         Authenticates server to the client
         --- 
@@ -32,49 +41,26 @@ class Client:
         print(initial_conn_string)
 
         # return initial_conn_string
-        # conn.send(initial_conn_string)
-        msg,integrity = conn.recv()
+        # self.socket.send(len(initial_conn_string))
+        self.socket.send(initial_conn_string)
+        length = self.bytes_to_int(self.socket.recv(2))
+        msg = self.socket.recv(length)
+        integrity = msg[32:]
+        msg = msg[:32]
 
         #signature verification, if wrong throws a value error
         # TODO: catch for Value Errors
+        # try:
         pss.new(rsa_public_key).verify(SHA256.new(msg), integrity)
-
-        sha = SHA256.new("Alice".encode() + Na.bytes).digest()
+        # except ValueError as e:
+        #     print("Integrity cannot be verified\n Authentication unsuccessful" , str(e))
+        sha = SHA256.new("Bob".encode() + Na.bytes).digest()
         Nb =  unpad(strxor(sha,msg),32)
         session_key = strxor(Na,Nb)
 
         return session_key
 
-        
-    def encrypt_file(self,public_key, file_data):
-
-        #compress the data first
-        file_data = zlib.compress(file_data)
-        #will encrypt and decrypt chunks at a time
-        chunk_size = 470
-        offset = 0
-        end_loop = False
-        encrypted = ""
-        while not end_loop:
-            #The chunk
-            chunk = file_data[offset:offset + chunk_size]
-
-            #If the data chunk is less then the chunk size, then we need to add
-            #padding with " ". This indicates the we reached the end of the file
-            #so we end loop here
-            if len(chunk) % chunk_size != 0:
-                end_loop = True
-                chunk += " " * (chunk_size - len(chunk))
-
-            #Append the encrypted chunk to the overall encrypted file
-            encrypted += rsa_key.encrypt(chunk)
-
-            #Increase the offset by chunk size
-            offset += chunk_size
-
-        #Base 64 encode the encrypted file
-        # return base64.b64encode(encrypted)
-
+    
     def bytes_to_int(self,data):
         """
             Converts bytes to integer
@@ -87,14 +73,14 @@ class Client:
             result += result*256 + int(b)
         return result
 
-    def int_to_bytes(self,data):
+    def int_to_bytes(self,data,length=16):
         """
             Converts integer to bytes
 
             Returns:
                 bytes 
         """
-        return data.to_bytes(16,byteorder='big')
+        return data.to_bytes(length,byteorder='big')
 
     def initiate_connection(self):
         """
@@ -107,83 +93,134 @@ class Client:
         s = socket.socket()
         s.connect(("localhost",5543))
         print("Connection established")
-        return s
+        self.socket = s
     
-    def send_seq_num(self, conn, key1, key2):
+    def send_seqA_num(self):
         """
         Generates random sequence number and send it as 
         keyed hash
         msg1 : send seq number using keyed hash  
         msg2: for server side to check integrity of msg1
         """
+        # FIXME: seqA and seqB 32 bits? step4 include seqA in hash?
         seq = random.randint(1000000000,9999999999)
-        seq_bytes = seq.to_bytes(32,byteorder='big')
-        msg = key1+ "Alice".encode()
+        seq_bytes = self.int_to_bytes(seq,32)
+        msg = "Alice".encode()+self.k1
         sha = SHA256.new(msg)
         msg = strxor(sha.digest(),seq_bytes)
-        
-        sha_integrity = SHA256.new(msg+key2)
+        sha_integrity = SHA256.new(msg+self.k2)
         #send the message and hash of message(32 bytes each)
-        conn.send(msg)
-        conn.send(sha_integrity.digest())
+        self.socket.send(msg)
+        self.socket.send(sha_integrity.digest())
+        return seq
     
-    def recv_seq_one(self, recv_msg1, key1, key2):
+    def recv_seqB(self, msg):
         '''
         Calculates the server side initial seq number 
         returns the server seq number as int
         '''
-        msg = key1 + "Bob".encode()
-        sha = SHA256.new(msg)
+        integrity = msg[33:]
+        msg = msg[:32]
+        sha_integrity = SHA256.new(msg+self.k2)
+        if  not sha_integrity.digest() == integrity:
+            print('message is tampered')
+        key_string = "Bob".encode()+self.k1
+        sha = SHA256.new(key_string)
         #received seq number in bytes 
-        recv_seq = strxor(recv_msg1 , sha.digest())
+        recv_seq = strxor(msg , sha.digest())
         seq = self.bytes_to_int(recv_seq)
         #verified till previous line ; we got back seq number
         return seq
 
+       
+    def close_connection(self):
+        self.socket.close()
 
-    def recv_seq_two(self ,recv_msg2,recv_msg1,  key2):
-        """
-        Checks integrity of incoming message 
-        """
 
-        sha_integrity = SHA256.new(recv_msg1+key2)
-        return  sha_integrity.digest()== recv_msg2
-        #verified 
-
+    def send_file(self,path):
+        # TODO: File transfer gets corrupted and the file retransmission is required in the middle of 
+        # exchange
         
-    
+        #send 3rd message
+        seqA = self.send_seqA_num()
+        #receive 4th message
+        recv_seqB_msg = self.socket.recv(64)
+        seqB = self.recv_seqB(recv_seqB_msg)
+        f_file = open(path, 'rb')
+        file_data= f_file.read()
+        f_file.close()
+        #compress the data first
+        # file_data = zlib.compress(file_data)
+        #will encrypt and decrypt chunks at a time
+        chunk_size = 32
+        key_string = "Alice".encode()+self.k1
+        offset = 0
+        end_loop = False
 
+        while not end_loop:
+            #The chunk
+            chunk = file_data[offset:offset + chunk_size]
+            trial_count = 2
+            while trial_count > 0 and trial_count < 2:
+                #If the data chunk is less then the chunk size, then we need to add
+                #padding with " ". This indicates the we reached the end of the file
+                #so we end loop here
+                if len(chunk) % chunk_size != 0:
+                    end_loop = True
+                    chunk += pad(chunk,chunk_size - len(chunk))
 
+                # Encryption using SHA
+                encrypted_msg = strxor(SHA256.new(key_string+ self.int_to_bytes(seqA)),chunk)
+                self.socket.send(len(chunk))
+                self.socket.send(encrypted_msg)
+                integrity = SHA256.new(encrypted_msg+self.int_to_bytes(k2))
+                self.socket.send(integrity)
 
+                # TODO: receive message from server anc check for integrity
+                recv_msg = self.socket.recv(64)
+                integrity = SHA256.new(recv_msg[33:]+self.int_to_bytes(k2))
+                msg = strxor(SHA256.new("Bob".encode()+self.k1+self.int_to_bytes(seqB)),recv_msg[:32])
+                
+                if msg != "Ok".encode():
+                    trial_count -= 1
+                #Increase the offset by chunk size
+                seqA += 1
+                seqB += 1
 
+            if trial_count == 0:
+                end_loop = True
+            offset += chunk_size
 
+        #Base 64 encode the encrypted file
+        # return base64.b64encode(encrypted)
 
+    def receive_file(self):
+        pass
 
-        
-
-def __init__():
+if __name__ == '__main__':
     client = Client()
-    s = client.initiate_connection()
+    client.initiate_connection()
     f_pubk = open('public_key.pem', 'rb')
     public_key = f_pubk.read()
     f_pubk.close()
-    session_key = client.server_authentication(public_key,s)
-    session = client.bytes_to_int(session_key)
-    k1 = client.int_to_bytes(session + 2)
-    k2 = client.int_to_bytes(session + 5)
-    k3 = client.int_to_bytes(session + 7)
-    k4 = client.int_to_bytes(session + 9)
-    #send 3rd message
-    client.send_seq_num(s, k1,k2)
-    #receive 4th message
-    recv_seq_msg_one = s.recv(32)
-    recv_seq_msg_two = s.recv(32)
-    server_seq_int = client.recv_seq_one(recv_seq_msg_one, k1, k2)
-    integrity_check = client.recv_seq_two( recv_seq_msg_two,recv_seq_msg_one, k2 )
-    if integrity_check:
-        pass
-    else:
-        print('message is tampered')
+    try: 
+        session_key = client.server_authentication(public_key)
+        session = client.bytes_to_int(session_key)
+        k1 = client.int_to_bytes(session + 2)
+        k2 = client.int_to_bytes(session + 5)
+        k3 = client.int_to_bytes(session + 7)
+        k4 = client.int_to_bytes(session + 9)
+        client.set_keys(k1,k2,k3,k4)
+        exit_flag = False
+        while not exit_flag:
+            # TODO: Step 3 and 4 inside send_file and different sequence numbers for each file
+            client.send_file('input_directory/izuku.jpg')
+            exit_flag = input("Continue?") == "False"
+    except Exception as e:
+        print(str(e))
+    finally:
+        client.close_connection()
+
 # print('Encrypting file...')
 # rsa_key = RSA.importKey(public_key)
 # initial_string = rsa_key.encrypt(Na+"Alice")
@@ -195,10 +232,10 @@ def __init__():
 # fd.write(encrypted_data)
 # fd.close()
 
-# f = open ("/home/rik/netsec/Secure-File-Transfer-Application/input_directory/izuku.jpg", "rb")
+f = open("/home/rik/netsec/Secure-File-Transfer-Application/input_directory/izuku.jpg", "rb")
 # s.send('encrypted_img.jpg'.encode('utf-8'))
-# l = f.read(1024)
-# while (l):
-#     s.send(l)
-#     l = f.read(1024)
+l = f.read(1024)
+while (l):
+    s.send(l)
+    l = f.read(1024)
 # s.close()
--- server.py
+++ server.py
@@ -37,9 +37,10 @@ def generate_key():
     fd.write(public_key)
     fd.close()
 
-class Server(socket):
+class Server:
     
-    def __init__(self):
+    def __init__(self,socket):
+        self.socket = socket
         self.k1 = 0
         self.k2 = 0
         self.k3 = 0
@@ -51,7 +52,7 @@ class Server(socket):
         self.k3 = k3
         self.k4 = k4
 
-    def send_ack_initial_connection(self,encrypted_msg,private_key,conn):
+    def send_ack_initial_connection(self,private_key):
         """
         * Responds to the client connection
 
@@ -59,7 +60,7 @@ class Server(socket):
         rsa_private_key = RSA.importKey(private_key)
         rsa_key = PKCS1_OAEP.new(rsa_private_key)
         Nb = uuid.uuid4()
-        # msg = conn.receive(512)
+        encrypted_msg = self.socket.receive(512)
         msg = rsa_key.decrypt(encrypted_msg)
         name = msg[-5:]
         print(name)
@@ -67,20 +68,61 @@ class Server(socket):
         #     conn.close()
         Na = msg[:-5]
         print("Na recv", Na)
-        msg = name + Na
+        msg = "Bob".encode() + Na.bytes
         sha = SHA256.new(msg)
-        print("len sha ",len(sha.digest()) )
+        # print("len sha ",len(sha.digest()) )
         # print("len Nb ",len(Nb.encode()))
-        print("pad length ",len(pad(Nb.bytes,32)))
+        # print("pad length ",len(pad(Nb.bytes,32)))
         msg = strxor(sha.digest(),pad(Nb.bytes,32))
         integrity = pss.new(rsa_private_key).sign(SHA256.new(msg))
         length = len(msg) + len(integrity)
+        self.socket.send(self.int_to_bytes(length,2))
+        self.socket.send(msg)
+        self.socket.send(integrity)
         session = strxor(Na,Nb.bytes)
-        conn.send(length)
         return session
 
     # return msg
     # conn.send(rsa_key.encrypt(msg))
+
+
+    def send_seqA_num(self):
+        """
+        Generates random sequence number and send it as 
+        keyed hash
+        msg1 : send seq number using keyed hash  
+        msg2: for server side to check integrity of msg1
+        """
+        # FIXME: seqA and seqB 32 bits? step4 include seqA in hash?
+        seq = random.randint(1000000000,9999999999)
+        seq_bytes = self.int_to_bytes(seq)
+        msg = "Alice".encode()+self.k1
+        sha = SHA256.new(msg)
+        msg = strxor(sha.digest(),seq_bytes)
+        sha_integrity = SHA256.new(msg+self.k2)
+        #send the message and hash of message(32 bytes each)
+        self.socket.send(msg)
+        self.socket.send(sha_integrity.digest())
+        return seq
+    
+    def recv_seqB(self, msg):
+        '''
+        Calculates the server side initial seq number 
+        returns the server seq number as int
+        '''
+        integrity = msg[33:]
+        msg = msg[:32]
+        sha_integrity = SHA256.new(msg+self.k2)
+        if  not sha_integrity.digest() == integrity:
+            print('message is tampered')
+        key_string = "Bob".encode()+self.k1
+        sha = SHA256.new(key_string)
+        #received seq number in bytes 
+        recv_seq = strxor(msg , sha.digest())
+        seq = self.bytes_to_int(recv_seq)
+        #verified till previous line ; we got back seq number
+        return seq
+
     def bytes_to_int(self,data):
         """
             Converts bytes to integer
@@ -94,85 +136,80 @@ class Server(socket):
 
         return result
 
-    def int_to_bytes(self,data):
+    def int_to_bytes(self,data,length=16):
         """
             Converts integer to bytes
 
             Returns:
                 bytes 
         """
-        return data.to_bytes(16,byteorder='big')
-    def recv_seq_one (self, recv_msg1, key1): 
-        """
-        Retrieves the the sequence number of client 
-        Returns : client sequence number
-        """
-        msg = key1 + "Alice".encode()
-        sha = SHA256.new(msg)
-        #received seq number in bytes 
-        recv_seq = strxor(recv_msg1 , sha.digest())
-        seq = self.bytes_to_int(recv_seq)
-        #verified till previous line ; we got back seq number
-        return seq
+        return data.to_bytes(length,byteorder='big')
 
-
+    def close_connection(self):
+        self.socket = None
+    
+    def receive_file(self,path):
+        pass
+    
+    def send_file(self,path):
+        # TODO: File transfer gets corrupted and the file retransmission is required in the middle of 
+        # exchange
         
-    def recv_seq_two(self, recv_msg2,recv_msg1,  key2):
-        """
-        Checks integrity of incoming message 
-        """
-
-        sha_integrity = SHA256.new(recv_msg1+key2)
-        return  sha_integrity.digest()== recv_msg2
-        #verified 
-
-
-
-
-
-
-    def send_seq_num(self,sock,  key1, key2):
-        seq = random.randint(1000000000,9999999999)
-        seq_bytes = seq.to_bytes(32,byteorder='big')
-        msg = key1+ "Bob".encode()
-        sha = SHA256.new(msg)
-        msg = strxor(sha.digest(),seq_bytes)
-        sha_integrity = SHA256.new(msg+key2)
-        sock.send(msg)
-        sock.send(sha_integrity.digest())
-
-    def encrypt_file(self,public_key, file_data):
-        rsa_key = RSA.importKey(public_key)
-        rsa_key = PKCS1_OAEP.new(rsa_key)
-
+        #send 3rd message
+        seqA = self.send_seqA_num()
+        #receive 4th message
+        recv_seqB_msg = self.socket.recv(64)
+        seqB = self.recv_seqB(recv_seqB_msg)
+        f_file = open(path, 'rb')
+        file_data= f_file.read()
+        f_file.close()
         #compress the data first
-        file_data = zlib.compress(file_data)
+        # file_data = zlib.compress(file_data)
         #will encrypt and decrypt chunks at a time
-        chunk_size = 470 
-        offset = 0 
+        chunk_size = 32
+        key_string = "Bob".encode()+self.k1
+        offset = 0
         end_loop = False
-        encrypted = ""
+
         while not end_loop:
             #The chunk
             chunk = file_data[offset:offset + chunk_size]
+            trial_count = 2
+            while trial_count > 0 and trial_count < 2:
+                #If the data chunk is less then the chunk size, then we need to add
+                #padding with " ". This indicates the we reached the end of the file
+                #so we end loop here
+                if len(chunk) % chunk_size != 0:
+                    end_loop = True
+                    chunk += pad(chunk,chunk_size - len(chunk))
 
-            #If the data chunk is less then the chunk size, then we need to add
-            #padding with " ". This indicates the we reached the end of the file
-            #so we end loop here
-            if len(chunk) % chunk_size != 0:
+                # Encryption using SHA
+                encrypted_msg = strxor(SHA256.new(key_string+ self.int_to_bytes(seqA)),chunk)
+                self.socket.send(len(chunk))
+                self.socket.send(encrypted_msg)
+                integrity = SHA256.new(encrypted_msg+self.int_to_bytes(k2))
+                self.socket.send(integrity)
+
+                # TODO: receive message from server anc check for integrity
+                recv_msg = self.socket.recv(64)
+                integrity = SHA256.new(recv_msg[33:]+self.int_to_bytes(k2))
+                msg = strxor(SHA256.new("Alice".encode()+self.k1+self.int_to_bytes(seqB)),recv_msg[:32])
+                
+                if msg != "Ok".encode():
+                    trial_count -= 1
+                #Increase the offset by chunk size
+                seqA += 1
+                seqB += 1
+
+            if trial_count == 0:
                 end_loop = True
-                chunk += " " * (chunk_size - len(chunk))
-
-            #Append the encrypted chunk to the overall encrypted file
-            encrypted += rsa_key.encrypt(chunk)
-
-            #Increase the offset by chunk size
             offset += chunk_size
 
         #Base 64 encode the encrypted file
         # return base64.b64encode(encrypted)
 
-def __init__():
+
+if __name__ == '__main__':
     s = socket.socket()
     s.bind(("localhost",5543))
     s.listen(10)
@@ -184,25 +221,19 @@ def __init__():
         print("Connection accepted from ",address)
         server = Server(sock)
         # TODO: receive bytes from client
-        msg = sock.recv(32)
-        session_key = server.send_ack_initial_connection(msg,private_key,sock)
-        session = server.bytes_to_int(session_key)
-        k1 = server.int_to_bytes(session + 2)
-        k2 = server.int_to_bytes(session + 5)
-        k3 = server.int_to_bytes(session + 7)
-        k4 = server.int_to_bytes(session + 9)
-        #receive 3rd message 
-        recv_seq_msg_one = sock.recv(32)
-        recv_seq_msg_two = sock.recv(32)
-        client_seq_int = server.recv_seq_one(recv_seq_msg_one, k1)
-        integrity_check = server.recv_seq_two(recv_seq_msg_two, recv_seq_msg_one , k2)
-        if integrity_check:
-            pass
-        else:
-            print('message is tampered')
-        #send 4th message
-        server.send_seq_num(sock, k1, k2)
-
+        try:
+            session_key = server.send_ack_initial_connection(private_key)
+            session = server.bytes_to_int(session_key)
+            k1 = server.int_to_bytes(session + 2)
+            k2 = server.int_to_bytes(session + 5)
+            k3 = server.int_to_bytes(session + 7)
+            k4 = server.int_to_bytes(session + 9)
+            server.set_keys(k1,k2,k3,k4)
+            server.receive_file("./output_directory")
+        except Exception as e:
+            print(str(e))
+        finally:
+            server.socket.close()
 
 # generate_key()
 # while True:
